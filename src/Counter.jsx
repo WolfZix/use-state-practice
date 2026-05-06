f8fa268bb6c3ef29ccf2c2806f5e61d048fcc8a1
Patryk Domaszek <patryk.domaszek41@gmail.com>
2026-05-06 16:17:47 +0200

    project created

--- src/Counter.jsx
+++ src/Counter.jsx
@@ -2,14 +2,15 @@ import React, { useState } from 'react';
 
 function Counter() {
     const [count, setCount] = useState(0);
+
     const increment = () => {
         setCount(count + 1);
     }
-    const decrement = () => {
-        setCount(count - 1);
-    }
     const reset = () => {
         setCount(0);
+    }
+    const decrement = () => {
+        setCount(count - 1);
     }
 
     return (
